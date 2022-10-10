272296b7749dacf8432ad68b454e517e56eb6cbf
cgreggtcd <98959891+cgreggtcd@users.noreply.github.com>
2022-10-10 10:47:42 +0100

    Add subtraction and power.

--- Choose.java
+++ Choose.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 //docker build -t java_choose_image .
 //docker run -it --rm --name choose_container java_choose_image
 
@@ -7,18 +8,18 @@ public class Choose {
         Scanner input = new Scanner( System.in );
         boolean exit = false;
         while(!exit) {
-            System.out.println("What would you like to do? 1 to add two integers, 2 to multiply two integers, and 3 to exit.");
+            System.out.println("What would you like to do? 1 to add two integers, 2 to multiply two integers, 3 to subtract two integers, 4 to calculate one integer to the power of another, and 5 to exit.");
             if(!input.hasNextInt()) {
-                System.out.println("Input one of 1, 2, 3.");
+                System.out.println("Input one of 1, 2, 3, 4, 5.");
                 input.next();
                 continue;
             }
             int selection = input.nextInt();
-            if (selection == 3){
+            if (selection == 5){
                 break;
             }
-            if(selection < 1 || selection > 3){
-                System.out.println("Input one of 1, 2, 3.");
+            if(selection < 1 || selection > 5){
+                System.out.println("Input one of 1, 2, 3, 4, 5.");
                 continue;
             }
             System.out.println("Input two integers");
@@ -38,12 +39,13 @@ public class Choose {
                 case 2:
                     result = int1 * int2;
                     break;
+                case 3:
+                  result = int1 - int2;
+                  break;
                 default:
-                    exit = true;
+                  result = (int) Math.pow((double)int1, (double)int2);
             }
-            if(result != -1){
-                System.out.println("Result is: "+ result);
-            }
+            System.out.println("Result is: "+ result);
         }
         input.close();
     }
